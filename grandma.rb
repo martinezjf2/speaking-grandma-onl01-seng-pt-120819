40ba0ca04789c7b6ef73ef32a03ce01ee722aba1
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-12 05:17:37 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,58 @@
+
+
+
+  if speak_to_grandma == "I LOVE YOU TOO GRANDMA!"
+    puts "I LOVE YOU PUMPKIN!"
+ 
+  elsif speak_to_grandma == "Hi,Nana, how are you?"
+    puts "HUH?! SPEAK UP SONNY!"
+  
+  elsif speak_to_grandma == "Hi!"
+    puts "HUH?! SPEAK UP, SONNY!"
+
+  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+    puts "NO, NOT SINCE 1938!"
+  
+  else speak_to_grandma == "WHAT?"
+    puts "NO, NOT SINCE 1938!"
+end
+
+speak_to_grandma
+
+# describe '#speak_to_grandma' do
+#   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
+#     expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+#   end
+
+#   it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
+#     expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
+#   end
+
+#   it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
+#     expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
+#   end
+
+#   it 'responds with NO, NOT SINCE 1938! when she can hear you' do
+#     expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
+#   end
+
+#   it 'responds with NO, NOT SINCE 1938! for a second time' do
+#     expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
+#   end
+
+# end
+
+
+
+
+
+
+
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
