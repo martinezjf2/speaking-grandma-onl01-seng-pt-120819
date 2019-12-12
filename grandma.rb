2b3f38341952a74af0bd734a3b65d7b97aafd316
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-12 23:56:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@
 def speak_to_grandma(string)
   
   if string == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
  
    elsif string == "Hi Nana, how are you?"
       puts "HUH?! SPEAK UP, SONNY!"
