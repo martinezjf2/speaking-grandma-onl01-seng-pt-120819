01dbd8dfe0d54b43dc1219c22f6e4b498db5ce7c
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-12 05:37:53 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,23 +1,23 @@
 
-
-
-  if speak_to_grandma == "I LOVE YOU TOO GRANDMA!"
-    puts "I LOVE YOU PUMPKIN!"
+def speak_to_grandma(string)
+  
+  if speak_to_grandma("I LOVE YOU GRANDMA!")
+      puts "I LOVE YOU PUMPKIN!"
  
-  elsif speak_to_grandma == "Hi,Nana, how are you?"
-    puts "HUH?! SPEAK UP SONNY!"
+   elsif speak_to_grandma("Hi,Nana, how are you?")
+      puts "HUH?! SPEAK UP SONNY!"
   
-  elsif speak_to_grandma == "Hi!"
-    puts "HUH?! SPEAK UP, SONNY!"
+   elsif speak_to_grandma("Hi!")
+      puts "HUH?! SPEAK UP, SONNY!"
 
-  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
-    puts "NO, NOT SINCE 1938!"
+    elsif speak_to_grandma("WHAT DID YOU EAT TODAY?")
+      puts "NO, NOT SINCE 1938!"
   
-  else speak_to_grandma == "WHAT?"
-    puts "NO, NOT SINCE 1938!"
+    else speak_to_grandma("WHAT?")
+      puts "NO, NOT SINCE 1938!"
+  end
 end
 
-speak_to_grandma
 
 # describe '#speak_to_grandma' do
 #   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
