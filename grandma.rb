14075eab6a5dd98ae108cf9e001fd82cf487595f
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-12 05:47:43 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,8 @@ def speak_to_grandma(string)
   if speak_to_grandma("I LOVE YOU GRANDMA!")
       puts "I LOVE YOU PUMPKIN!"
  
-   elsif speak_to_grandma("Hi,Nana, how are you?")
-      puts "HUH?! SPEAK UP SONNY!"
+   elsif speak_to_grandma("Hi Nana, how are you?")
+      puts "HUH?! SPEAK UP, SONNY!"
   
    elsif speak_to_grandma("Hi!")
       puts "HUH?! SPEAK UP, SONNY!"
